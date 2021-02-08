35ae7ef905a6d4c8266f5594c625660a3b192939
Son Chau <sonchau24@yahoo.com>
2021-02-08 14:30:14 +1100

    [#39] handling event

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -16,15 +16,20 @@ class Cart extends React.Component<Props, State> {
     };
   }
 
+  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    // if ((e.target as HTMLElement).nodeName === 'SPAN') {
+    //   (e.target as HTMLSpanElement).;
+    // }
+    this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
+  };
+
   render() {
     return (
       <div className={CartCSS.cartContainer}>
         <button
           className={CartCSS.button}
           type="button"
-          onClick={() => {
-            this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
-          }}
+          onClick={this.handleClick}
         >
           <FiShoppingCart />
           <span>2 pizza(s)</span>
