d37b40442fefb0c610b3d9caa09e352e1d1934b9
Son Chau <sonchau24@yahoo.com>
2021-02-09 10:44:05 +1100

    [#40] using React context and useState hook

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FiShoppingCart } from 'react-icons/fi';
 import CartCSS from './Cart.module.css';
+import { AppStateContext } from './AppState';
 
 interface Props {}
 
@@ -25,27 +26,38 @@ class Cart extends React.Component<Props, State> {
 
   render() {
     return (
-      <div className={CartCSS.cartContainer}>
-        <button
-          className={CartCSS.button}
-          type="button"
-          onClick={this.handleClick}
-        >
-          <FiShoppingCart />
-          <span>2 pizza(s)</span>
-        </button>
-        <div
-          className={CartCSS.cartDropDown}
-          style={{
-            display: this.state.isOpen ? 'block' : 'none',
-          }}
-        >
-          <ul>
-            <li>Napoletana</li>
-            <li>Marinara</li>
-          </ul>
-        </div>
-      </div>
+      <AppStateContext.Consumer>
+        {(state) => {
+          return (
+            <div className={CartCSS.cartContainer}>
+              <button
+                className={CartCSS.button}
+                type="button"
+                onClick={this.handleClick}
+              >
+                <FiShoppingCart />
+                <span>{state.cart.items.length} pizza(s)</span>
+              </button>
+              <div
+                className={CartCSS.cartDropDown}
+                style={{
+                  display: this.state.isOpen ? 'block' : 'none',
+                }}
+              >
+                <ul>
+                  {state.cart.items.map((item) => {
+                    return (
+                      <li key={item.id}>
+                        {item.name} &times; {item.quantity}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          );
+        }}
+      </AppStateContext.Consumer>
     );
   }
 }
