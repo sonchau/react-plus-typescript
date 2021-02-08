0bbe199ca07057ac1888eaa8bfe9509e01741d72
Son Chau <sonchau24@yahoo.com>
2021-02-08 14:10:32 +1100

    [#37] import SVG

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,17 @@ module.exports = {
           { loader: 'css-loader', options: { modules: true } },
         ],
       },
+      {
+        test: /\.svg$/,
+        loader: '@svgr/webpack',
+        options: {
+          svgoConfig: {
+            plugins: {
+              removeViewBox: false,
+            },
+          },
+        },
+      },
     ],
   },
   plugins: [
