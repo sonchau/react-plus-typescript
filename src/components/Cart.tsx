9104de80bc9aa2ecb9cd1025c98555eeab4f102f
Son Chau <sonchau24@yahoo.com>
2021-02-09 11:06:06 +1100

    [#41] replace setState with useReducer

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -28,6 +28,9 @@ class Cart extends React.Component<Props, State> {
     return (
       <AppStateContext.Consumer>
         {(state) => {
+          const itemsCount = state.cart.items.reduce((sum, item) => {
+            return sum + item.quantity;
+          }, 0);
           return (
             <div className={CartCSS.cartContainer}>
               <button
@@ -36,7 +39,7 @@ class Cart extends React.Component<Props, State> {
                 onClick={this.handleClick}
               >
                 <FiShoppingCart />
-                <span>{state.cart.items.length} pizza(s)</span>
+                <span>{itemsCount} pizza(s)</span>
               </button>
               <div
                 className={CartCSS.cartDropDown}
